f17aabbad8584851674002be3331c8d512c5b352
Hygi1 <ppravu@mail.ru>
2025-05-22 16:11:18 +0700

    2 задачка по js

--- Skript.js
+++ Skript.js
@@ -60,3 +60,22 @@ let password = "пароль";
 let userInput = prompt("Введите пароль");
 if (userInput === password) console.log("Пароль введен верно");
 else console.log("Пароль введен неправильно");
+
+let c = 2;
+if (c > 0 && c < 10) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
+}
+c = 0;
+if (c > 0 && c < 10) console.log("Верно");
+else console.log("Неверно");
+c = 10;
+if (c > 0 && c < 10) console.log("Верно");
+else console.log("Неверно");
+c = -3;
+if (c > 0 && c < 10) console.log("Верно");
+else console.log("Неверно");
+c = 2;
+if (c > 0 && c < 10) console.log("Верно");
+else console.log("Неверно");
