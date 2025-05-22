fd288a34dcc796f0e5b30ee3f606d9477bf2814b
Hygi1 <ppravu@mail.ru>
2025-05-22 16:25:54 +0700

    5 задачка по js

--- Skript.js
+++ Skript.js
@@ -91,3 +91,30 @@ if (d > 100 || e > 100) {
 let a = "2";
 let b = "3";
 alert(+a + +b);
+
+let monthNumber = 12;
+
+switch (monthNumber) {
+  case 12:
+  case 1:
+  case 2:
+    console.log("Зима");
+    break;
+  case 3:
+  case 4:
+  case 5:
+    console.log("Весна");
+    break;
+  case 6:
+  case 7:
+  case 8:
+    console.log("Лето");
+    break;
+  case 9:
+  case 10:
+  case 11:
+    console.log("Осень");
+    break;
+  default:
+    console.log("Ошибка");
+}
