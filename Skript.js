39af773bbc20769889971d4b10ba7aa495d78c2a
Hygi1 <ppravu@mail.ru>
2025-05-22 16:19:46 +0700

    3 задачка по js

--- Skript.js
+++ Skript.js
@@ -79,3 +79,11 @@ else console.log("Неверно");
 c = 2;
 if (c > 0 && c < 10) console.log("Верно");
 else console.log("Неверно");
+
+let d = 24;
+let e = 46;
+if (d > 100 || e > 100) {
+  console.log("Верно");
+} else {
+  console.log("Неверно");
+}
