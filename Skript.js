9d987278c1f3ef9520ba891ad6f3261cdfedfb8e
Hygi1 <ppravu@mail.ru>
2025-10-13 21:59:30 +0700

    4 задачка по массивам

--- Skript.js
+++ Skript.js
@@ -1,4 +1,14 @@
-const array = [1, 5, 4, 10, 0, 3];
+const rows = 3;
+const cols = 3;
 
-const result = numbers.join(" ");
-console.log(result);
+const matrix = [];
+
+for (let i = 0; i < rows; i++) {
+  matrix[i] = [];
+
+  for (let j = 0; j < cols; j++) {
+    matrix[i][j] = 1;
+  }
+}
+
+console.log(JSON.stringify(matrix, null, 2));
