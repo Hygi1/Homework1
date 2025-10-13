b901cea9d75d750c377395ffd0ef8cfc5618fe46
Hygi1 <ppravu@mail.ru>
2025-10-13 22:20:17 +0700

    15 задание массивы

--- Skript.js
+++ Skript.js
@@ -1,12 +1,20 @@
-const randomArray = Array.from({ length: 10 }, () =>
-  Math.floor(Math.random() * 11)
+const randomArray = Array.from(
+  { length: 6 },
+  () => Math.floor(Math.random() * 10) + 1
 );
 
-function getEvenNumbers(arr) {
-  return arr.filter((num) => num % 2 === 0);
+function calculateAverage(arr) {
+  if (arr.length === 0) return 0;
+
+  const sum = arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+
+  return sum / arr.length;
 }
 
-const evenNumbers = getEvenNumbers(randomArray);
+const average = calculateAverage(randomArray);
 
 console.log("Исходный массив:", randomArray);
-console.log("Массив четных чисел:", evenNumbers);
+console.log("Среднее арифметическое:", average.toFixed(2));
