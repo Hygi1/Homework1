6f5c26b7046c8513ae2b627066dc5b1823634dfa
Hygi1 <ppravu@mail.ru>
2025-10-13 21:55:15 +0700

    2 задачка по массивам

--- Skript.js
+++ Skript.js
@@ -1,9 +1,4 @@
 const array = [1, 5, 4, 10, 0, 3];
 
-for (let i = 0; i < array.length; i++) {
-  if (array[i] === 10) {
-    console.log(array[i]);
-    break;
-  }
-  console.log(array[i]);
-}
+const index = array.indexOf(4);
+console.log(index);
