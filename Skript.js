14b714cd59372ecbb6c858468202eccc9e5fcd60
Hygi1 <ppravu@mail.ru>
2025-10-13 22:12:22 +0700

    10 задачка по массивам

--- Skript.js
+++ Skript.js
@@ -1,7 +1,15 @@
-const array = [
-  [1, 2, 3],
-  [4, 5, 6],
+const numbers = [
+  Math.floor(Math.random() * 10) + 1,
+  Math.floor(Math.random() * 10) + 1,
+  Math.floor(Math.random() * 10) + 1,
+  Math.floor(Math.random() * 10) + 1,
+  Math.floor(Math.random() * 10) + 1,
 ];
 
-const result1 = [].concat(...array);
-console.log(result1);
+console.log("Исходный массив:", numbers);
+
+for (let i = 0; i < numbers.length - 1; i++) {
+  const sum = numbers[i] + numbers[i + 1];
+
+  console.log(`Сумма элементов ${numbers[i]} и ${numbers[i + 1]} = ${sum}`);
+}
