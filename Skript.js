5eaa07e606c5c0fac44a4f748fc422411df53795
Hygi1 <ppravu@mail.ru>
2025-10-13 22:08:09 +0700

    8 задание по массивам

--- Skript.js
+++ Skript.js
@@ -1,15 +1,3 @@
-const numbers = [9, 8, 7, 6, 5];
-
-const userInput = prompt("Угадайте число из массива (от 5 до 9):");
-
-if (userInput === null) {
-  alert("Вы отменили игру");
-} else {
-  const userNumber = parseInt(userInput, 10);
-
-  if (numbers.includes(userNumber)) {
-    alert("Угадал!");
-  } else {
-    alert("Не угадал");
-  }
-}
+const str = "abcdef";
+const reversed = str.split("").reverse().join("");
+console.log(reversed);
